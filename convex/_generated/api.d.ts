6982556aa932748b5c99d0994c61e7eb38e2b75d
lucasfth <online@lucashanson.dk>
2026-03-17 13:52:32 +0100

    Include convex generated schema

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,57 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as activities from "../activities.js";
+import type * as context from "../context.js";
+import type * as http from "../http.js";
+import type * as jobs from "../jobs.js";
+import type * as messages from "../messages.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  activities: typeof activities;
+  context: typeof context;
+  http: typeof http;
+  jobs: typeof jobs;
+  messages: typeof messages;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
