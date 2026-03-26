d98c05f0c4f0caaca07b0b70c802425190ad10a7
lucasfth <online@lucashanson.dk>
2026-03-26 07:05:38 +0100

    Add add apple icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,22 @@ const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '')
 export default defineNuxtConfig({
   modules: ['nuxt-auth-utils', '@nuxtjs/tailwindcss'],
 
+  app: {
+    head: {
+      title: 'Claus Dashboard',
+      meta: [
+        { name: 'description', content: 'A dashboard for Claus' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
+    },
+  },
+
   runtimeConfig: {
     session: {
       password: env.NUXT_SESSION_PASSWORD ?? process.env.NUXT_SESSION_PASSWORD ?? '',
