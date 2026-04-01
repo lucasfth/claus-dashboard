3cd70a3cb10a3afde3bd754cf37330331e0b7f77
Lucas Hanson <94063597+lucasfth@users.noreply.github.com>
2026-04-01 16:05:02 +0200

    feat: switch from GitHub OAuth to Password provider

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,6 @@
-import GitHub from '@auth/core/providers/github'
+import { Password } from '@convex-dev/auth/providers/Password'
 import { convexAuth } from '@convex-dev/auth/server'
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [GitHub],
+  providers: [Password],
 })
